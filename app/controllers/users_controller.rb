94c481f03ac1a4eb213b1d58aed51efe32fe21f4
YutoAoki <frcsnt@gmail.com>
2018-09-09 13:28:54 +0900

    girst master commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,8 @@ class UsersController < ApplicationController
     end
   end
 
+  # git test
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
